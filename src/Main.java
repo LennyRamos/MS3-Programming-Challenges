0e0adac7a2fd566fcc3a2d9a7298d7a985a9692c
LennyRamos <tuf18062@temple.edu>
2017-04-04 11:16:30 -0400

    Just made some comment in main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-/**
- * Created by Lenny Ramos on 4/4/2017.
+/**---------------------------------------------------**
+ *
+ *              Created by Lenny Ramos
+ *                   April 4, 2017
+ *
+ *                       Goals:
+ *
+ *   1) Will open a .csv file, parse the data and insert
+ *      it into a SQLlite In-Memory database
+ *
+ *   2) Data sets can be extremely large, so optimize for
+ *      efficiency. (Perhaps use of threads to delegate task)
+ *
+ *   3) Need to verify that each record has 10 data elements.
+ *      If records does not meet the requirement then I need to
+ *      write that record to a bad-data-<timestamp>.csv file.
+ *      elements with commas (,) will be double quoted.
+ *
+ *   4) Finally need a log file with # of records received
+ *                                   # of records successful
+ *                                   # of records failed
+ *
+ *----------------------------------------------------**
  */
+
+
 public class Main {
 }
